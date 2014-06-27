41709650501c79da3980aac1782c7625bd00161f
kmakantasis <konst.makantasis@gmail.com>
2014-06-27 15:31:30 +0300

    Implements a method for loading datasets

--- loadDataset.py
+++ loadDataset.py
@@ -6,6 +6,7 @@ import gzip
 import os
 import theano
 import theano.tensor as T
+import scipy.io
 
 
 def load_data(dataset):
@@ -60,3 +61,54 @@ def load_data(dataset):
     rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y), (test_set_x, test_set_y)]
     
     return rval
+    
+    
+def load_multi():
+    
+    l_file = scipy.io.loadmat('multi_data/test_samples.mat')
+    test_samples = l_file['test_samples']
+    l_file = scipy.io.loadmat('multi_data/test_labels.mat')
+    test_labels = l_file['test_labels']
+
+    l_file = scipy.io.loadmat('multi_data/valid_samples.mat')
+    valid_samples = l_file['valid_samples']
+    l_file = scipy.io.loadmat('multi_data/valid_labels.mat')
+    valid_labels = l_file['valid_labels']
+
+    l_file = scipy.io.loadmat('multi_data/train_samples.mat')
+    train_samples = l_file['train_samples']
+    l_file = scipy.io.loadmat('multi_data/train_labels.mat')
+    train_labels = l_file['train_labels']
+
+
+    train_set = [train_samples, np.reshape(train_labels, (29943,))]
+    valid_set = [valid_samples, np.reshape(valid_labels, (6416,))]
+    test_set = [test_samples, np.reshape(test_labels, (6416,))]    
+    
+    def shared_dataset(data_xy, borrow=False):
+        """ Function that loads the dataset into shared variables
+
+        The reason we store our dataset in shared variables is to allow
+        Theano to copy it into the GPU memory (when code is run on GPU).
+        Since copying data into the GPU is slow, copying a minibatch everytime
+        is needed (the default behaviour if the data is not in a shared
+        variable) would lead to a large decrease in performance.
+        """
+        data_x, data_y = data_xy
+        shared_x = theano.shared(np.asarray(data_x,
+                                               dtype=theano.config.floatX),
+                                 borrow=borrow)
+        shared_y = theano.shared(np.asarray(data_y,
+                                               dtype=theano.config.floatX),
+                                 borrow=borrow)
+       
+        return shared_x, T.cast(shared_y, 'int32')
+
+    test_set_x, test_set_y = shared_dataset(test_set)
+    valid_set_x, valid_set_y = shared_dataset(valid_set)
+    train_set_x, train_set_y = shared_dataset(train_set)
+
+    rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y), (test_set_x, test_set_y)]
+    
+    return rval
+    
