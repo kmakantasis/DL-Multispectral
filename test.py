c37dc26253eae726963c8d78a35f01e0a248b344
kostas <konst.makantasis@gmail.com>
2014-06-27 18:09:23 +0300

    Simple script to test various code blocks

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from loadDataset import load_data, load_multi
+
 import theano
 import theano.tensor as T
+from loadDataset import load_data
+from LogisticRegression import LogisticRegression
 
-batch_size = 600
-dataset='mnist.pkl.gz'
-datasets, train_set, valid_set, test_set = load_data(dataset)
 
+batch_size = 20
+datasets = load_data(dataset)
 
 train_set_x, train_set_y = datasets[0]
 valid_set_x, valid_set_y = datasets[1]
@@ -16,37 +17,26 @@ test_set_x, test_set_y = datasets[2]
 n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
 n_valid_batches = valid_set_x.get_value(borrow=True).shape[0] / batch_size
 n_test_batches = test_set_x.get_value(borrow=True).shape[0] / batch_size
+    
+print '... building the model'
 
-import scipy.io
-import numpy as np
-
-l_file = scipy.io.loadmat('multi_data/test_samples.mat')
-test_samples = l_file['test_samples']
-l_file = scipy.io.loadmat('multi_data/test_labels.mat')
-test_labels = l_file['test_labels']
-
-l_file = scipy.io.loadmat('multi_data/valid_samples.mat')
-valid_samples = l_file['valid_samples']
-l_file = scipy.io.loadmat('multi_data/valid_labels.mat')
-valid_labels = l_file['valid_labels']
-
-l_file = scipy.io.loadmat('multi_data/train_samples.mat')
-train_samples = l_file['train_samples']
-l_file = scipy.io.loadmat('multi_data/train_labels.mat')
-train_labels = l_file['train_labels']
+    
+index = T.lscalar()  
 
 
-train_set = [train_samples, np.reshape(train_labels, (145180,))]
-valid_set = [valid_samples, np.reshape(valid_labels, (31111,))]
-test_set = [test_samples, np.reshape(test_labels, (31110,))]
+x = T.matrix('x') 
+y = T.ivector('y') 
 
-data_x, data_y = valid_set
-shared_x = theano.shared(np.asarray(data_x, dtype=theano.config.floatX), borrow=False)
-shared_y = theano.shared(np.asarray(data_y, dtype=theano.config.floatX), borrow=True)
-T.cast(shared_y, 'int32')
+classifier = LogisticRegression(x, y, n_in=784, n_out=10)
 
+#cost = T.log(classifier.p_y_given_x)[T.arange(y.shape[0]), y]
+cost = T.log(classifier.p_y_given_x)[T.arange(y.shape[0]), y]
 
 
-datasets = load_multi(train_set, valid_set, test_set)
-
-
+train_model = theano.function(inputs=[index],
+                                  outputs=cost,
+                                  givens={x: train_set_x[index * batch_size:(index + 1) * batch_size],
+                                          y: train_set_y[index * batch_size:(index + 1) * batch_size]})
+                                          
+                                          
+loss = train_model(1)
