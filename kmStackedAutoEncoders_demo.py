529ca9e65635ac04a48e22d524d100ed2bf04f9b
kmakantasis <konst.makantasis@gmail.com>
2014-06-30 16:19:20 +0300

    Stacked autoencoders demo

--- kmStackedAutoEncoders_demo.py
+++ kmStackedAutoEncoders_demo.py
@@ -21,7 +21,7 @@ def StackedAutoEncoders_demo(finetune_lr=0.1, pretraining_epochs=10, pretrain_lr
     numpy_rng = np.random.RandomState(89677)
     print '... building the model'
     
-    sda = StackedAutoEncoders(numpy_rng, n_ins=103, hidden_layer_sizes=[206, 206, 206], n_outs=9)
+    sda = StackedAutoEncoders(numpy_rng, n_ins=103, hidden_layer_sizes=[309, 309, 309], n_outs=9)
   
   
     #########################
